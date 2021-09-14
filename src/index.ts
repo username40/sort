dd2886a5205a0966bcd871a609a7a99b96d25541
username40 <mr.v.e.romantsov@gmail.com>
2021-09-14 16:18:32 +0300

    beginner implementation of sort

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-console.log('HELLOOOOO!')
+class Sorter {
+    /*
+    collection: number[]
+
+    constructor(collection: number[]) {
+        this.collection = collection
+    }*/
+    constructor(public collection: number[]) {
+    }
+
+    sort():void {
+        const { length } = this.collection;
+
+        for (let i = 0; i < length; i++) {
+            for(let j = 0; j < length - i -1; j++) {
+                if (this.collection[j] > this.collection[j + 1]) {
+                    const leftHand = this.collection[j];
+                    this.collection[j] = this.collection[j + 1];
+                    this.collection[j + 1] = leftHand;
+                }
+            }
+        }
+    }
+}
+
+const sorter = new Sorter([-1,3,29, 11])
+sorter.sort()
+console.log(sorter.collection)
