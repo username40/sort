4a3c4934a7365b979aacee7cd1a1039a11503afa
username40 <mr.v.e.romantsov@gmail.com>
2021-10-12 21:53:20 +0300

    refactoring

--- build/index.js
+++ build/index.js
@@ -1,28 +1,4 @@
 "use strict";
-var Sorter = /** @class */ (function () {
-    /*
-    collection: number[]
-
-    constructor(collection: number[]) {
-        this.collection = collection
-    }*/
-    function Sorter(collection) {
-        this.collection = collection;
-    }
-    Sorter.prototype.sort = function () {
-        var length = this.collection.length;
-        for (var i = 0; i < length; i++) {
-            for (var j = 0; j < length - i - 1; j++) {
-                if (this.collection[j] > this.collection[j + 1]) {
-                    var leftHand = this.collection[j];
-                    this.collection[j] = this.collection[j + 1];
-                    this.collection[j + 1] = leftHand;
-                }
-            }
-        }
-    };
-    return Sorter;
-}());
 var sorter = new Sorter([-1, 3, 29, 11]);
 sorter.sort();
 console.log(sorter.collection);
--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,4 @@
-class Sorter {
-    /*
-    collection: number[]
 
-    constructor(collection: number[]) {
-        this.collection = collection
-    }*/
-    constructor(public collection: number[] | string) {
-    }
-
-    sort():void {
-        const { length } = this.collection;
-
-        for (let i = 0; i < length; i++) {
-            for(let j = 0; j < length - i -1; j++) {
-                if (this.collection instanceof Array ) {
-                    if (this.collection[j] > this.collection[j + 1]) {
-                        const leftHand = this.collection[j];
-                        this.collection[j] = this.collection[j + 1];
-                        this.collection[j + 1] = leftHand;
-                    }
-                }
-
-                if (typeof this.collection === 'string') {
-                }
-            }
-        }
-    }
-}
 
 const sorter = new Sorter([-1,3,29, 11])
 sorter.sort()
